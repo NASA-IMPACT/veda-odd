e017290cb1e872fc2c8b38ce0ca6845c0d5356df
Aimee Barciauskas <aimee@developmentseed.org>
2025-05-09 09:14:32 -0700

    Minor changes to scripts

--- nldas_benchmarking/nccs-to-s3/copy_files.sh
+++ nldas_benchmarking/nccs-to-s3/copy_files.sh
@@ -5,13 +5,27 @@ timechunks="06"
 export timechunks
 export base_url="https://portal.nccs.nasa.gov/lisdata_pub/NLDAS/.timechunk${timechunks}/"
 
-curl -s "https://portal.nccs.nasa.gov/lisdata_pub/NLDAS/.timechunk${timechunks}/" | \
-  grep -oP '(?<=href=")[^"]+\.nc(?=")' > nc_files_${timechunks}.txt
+curl -s $base_url | grep -oP '(?<=href=")[^"]+\.nc(?=")' > nc_files_${timechunks}.txt
+
+# install and configure rclone
+curl https://rclone.org/install.sh | sudo bash
+rclone config # quit
+cat <<EOF > ~/.config/rclone/rclone.conf
+[s3]
+type = s3
+provider = AWS
+env_auth = true
+region = us-west-2
+location_constraint = us-west-2
+acl = bucket-owner-full-control
+EOF
+
+sudo yum install -y parallel
 
 # stream copying files
-cat nc_files_${timechunks}.txt | parallel --ungroup -j 12 '
-  echo "Transferring: {}" >&2
+nohup bash -c 'cat nc_files_${timechunks}.txt | parallel --ungroup -j 12 "
+  echo \"Transferring: {}\" >&2
   rclone -P copyurl \
-    "https://portal.nccs.nasa.gov/lisdata_pub/NLDAS/.timechunk${timechunks}/{}" \
+    \"${base_url}/{}\" \
     s3:nasa-eodc-scratch/NLDAS/netcdf/.timechunk${timechunks}/{}
-'
+"' > rclone_transfer.log 2>&1 &
