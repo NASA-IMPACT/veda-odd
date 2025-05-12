f57f3503ef641393ee65bef440a06b82269b8da0
Aimee Barciauskas <aimee@developmentseed.org>
2025-05-12 09:19:32 -0700

    Changes to data moving

--- nldas_benchmarking/nccs-to-s3/copy_files.sh
+++ nldas_benchmarking/nccs-to-s3/copy_files.sh
@@ -1,38 +1,15 @@
 # ssh -i ~/.ssh/abarciauskas.pem ec2-user@instance-ip
 
 # write all nc files to a list file
-timechunks="06"
-export timechunks
-export base_url="https://portal.nccs.nasa.gov/lisdata_pub/NLDAS/.timechunk${timechunks}/"
+export timechunks="01"
+export base_url="https://portal.nccs.nasa.gov/lisdata_pub/NLDAS/.timechunk${timechunks}"
 
-curl -s $base_url | grep -oP '(?<=href=")[^"]+\.nc(?=")' > nc_files_${timechunks}.txt
+curl -s -L $base_url | grep -oP '(?<=href=")[^"]+\.nc(?=")' > nc_files_${timechunks}.txt
+mkdir ncfiles
 
-# install and configure rclone
-curl https://rclone.org/install.sh | sudo bash
-rclone config # quit
-cat <<EOF > ~/.config/rclone/rclone.conf
-[s3]
-type = s3
-provider = AWS
-env_auth = true
-region = us-west-2
-location_constraint = us-west-2
-acl = bucket-owner-full-control
-EOF
+sudo yum install parallel -y
+nohup bash -c "mkdir -p logs && cat nc_files_${timechunks}.txt | parallel --no-notice -j 4 'curl -L -o ncfiles/{} ${base_url}/{} 2>> logs/{}.log'" > output.log 2>&1 &
 
-sudo yum install -y parallel
-
-# stream copying files
-nohup bash -c 'cat nc_files_${timechunks}.txt | parallel --ungroup -j 2 "
-  echo \"Transferring: {}\" >&2
-  rclone -P copyurl \
-    \"${base_url}/{}\" \
-    s3:nasa-eodc-scratch/NLDAS/netcdf/.timechunk${timechunks}/{}
-    --low-level-retries 10 \
-    --retries 3 \
-    --transfers 4 \
-    --multi-thread-streams 4 \
-    --buffer-size 32M \
-    --timeout 2m \
-    --contimeout 1m
-"' > rclone_transfer.log 2>&1 &
+aws configure set default.s3.max_concurrent_requests 50
+aws configure set default.s3.multipart_chunksize 64MB
+nohup bash -c "aws s3 cp --recursive finished/ s3://nasa-eodc-scratch/NLDAS/netcdf/.timechunk${timechunks}/" > cp_to_s3_output.log 2>&1 &
--- nldas_benchmarking/nccs-to-s3/launch_instance.sh
+++ nldas_benchmarking/nccs-to-s3/launch_instance.sh
@@ -11,41 +11,17 @@
 # aws ec2 authorize-security-group-ingress --group-id "sg-preview-1" --ip-permissions '{"IpProtocol":"tcp","FromPort":22,"ToPort":22,"IpRanges":[{"CidrIp":"$MYIP"}]}'
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-aws ec2 run-instances --image-id "ami-07b0c09aab6e66ee9" --instance-type "t3.medium" \
+aws ec2 run-instances --image-id "ami-07b0c09aab6e66ee9" --instance-type "t3.xlarge" \
   --key-name "abarciauskas" \
   --network-interfaces '{"AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-09774292ec4131f6f"]}' \
   --iam-instance-profile "Arn=arn:aws:iam::${ACCOUNT_ID}:instance-profile/ec2-s3-access-role" \
   --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"aimee-nldas-data-copy"}]}' \
-  --user-data '#!/bin/bash
-    # Install rclone
-    curl https://rclone.org/install.sh | sudo bash
-
-    # Setup config
-    mkdir -p ~/.config/rclone
-    cat <<"EOF" > ~/.config/rclone/rclone.conf
-    [s3]
-    type = s3
-    provider = AWS
-    env_auth = true
-    region = us-west-2
-    location_constraint = us-west-2
-    acl = bucket-owner-full-control
-    EOF
-
-    export BASE_URL=https://portal.nccs.nasa.gov/lisdata_pub/NLDAS/.timechunk06
-    export FILENAME=NLDAS_FOR0010_H.A20010110.030.beta.nc
-    export URL=$BASE_URL/$FILENAME
-    export timechunks="06"
-    nohup bash -c "
-      echo \"Transferring: \${URL}\" >&2
-      rclone -P copyurl \"\${URL}\" \
-      s3:nasa-eodc-scratch/NLDAS/netcdf/.timechunk\${timechunks}/filename.nc \
-      --low-level-retries 10 \
-      --retries 3 \
-      --transfers 4 \
-      --multi-thread-streams 4 \
-      --buffer-size 32M \
-      --timeout 2m \
-      --contimeout 1m
-    " > rclone_transfer.log 2>&1 &
-  '
+  --block-device-mappings '[{
+    "DeviceName": "/dev/xvda",
+    "Ebs": {
+      "VolumeSize": 200,
+      "VolumeType": "gp3",
+      "DeleteOnTermination": true
+    }
+  }]' \
+  --count 1  
