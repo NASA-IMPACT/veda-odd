c3f3741a4cb08cf7c064aa3e8a958f536f93baa0
Aimee Barciauskas <aimee@developmentseed.org>
2025-05-16 21:38:06 +0000

    a lot of intermediate work

---
+++ nldas_benchmarking/01_create_intake_stac/test_helpers.py
@@ -0,0 +1,88 @@
+import random
+from datetime import datetime
+from typing import Union
+import s3fs
+import numpy as np
+import xarray as xr
+from log_monitor import S3FSLogMonitor
+from titiler.xarray.io import Reader
+
+def select_random_spatial_range(
+    spatial_size: Union[int, float],
+    precision: Union[int, float],
+    lat_range: list = [40, 62],
+    lon_range: list = [-125, -103],
+):
+    # select random latitude and longitude range by selecting a random integeter between
+    random_lat = random.uniform(lat_range[0] + spatial_size, lat_range[1])
+    random_lat_range = slice(round(random_lat - spatial_size, precision), round(random_lat, precision))
+    random_lon = random.uniform(lon_range[0] + spatial_size, lon_range[1])
+    random_lon_range = slice(round(random_lon - spatial_size, precision), round(random_lon, precision))
+    return random_lat_range, random_lon_range
+
+backend_open_kwargs = {
+    'engine': 'h5netcdf', # could also be netcdf4
+    'chunks': {}, # chunks={} loads the data with dask using the engine’s preferred chunk size, generally identical to the format’s chunk size
+    'backend_kwargs': {'driver_kwds': {'rdcc_nbytes': 0}}, # 0 bytes for raw data chunk cache
+    'cache': False, # If True, cache data loaded from the underlying datastore in memory as NumPy arrays when accessed to avoid reading from the underlying data- store multiple times. Defaults to True unless you specify the chunks argument to use dask, in which case it defaults to False. 
+    'decode_cf': False
+}
+s3fs_open_kwargs = {
+    'default_fill_cache': False,
+    'default_block_cache': 5 * 10**7, # 50MB is the default but setting here so I remember to configure this
+}
+
+def load_series(test_file: str, s3fsfs: s3fs.S3FileSystem, random_lat_range: slice, random_lon_range: slice):
+    monitor = S3FSLogMonitor()
+    
+    test_file_name = test_file.split('/')[-1]
+    print(f"starting test for {test_file_name}")
+    open_start_time = datetime.now()
+    ds = xr.open_dataset(
+        s3fsfs.open(f's3://{test_file}', **s3fs_open_kwargs),
+        **backend_open_kwargs
+    )
+    open_end_time = datetime.now()
+    da = ds.Tair
+    da.sel(lat=random_lat_range, lon=random_lon_range).load()
+    load_time = (datetime.now() - open_end_time).total_seconds()
+    open_time = (open_end_time - open_start_time).total_seconds()
+    ds.close()
+    test_info = {
+        'filename': test_file_name,
+        'chunk_shape': da.encoding['preferred_chunks'],
+        'chunk_size (MB)': np.prod(da.encoding['chunksizes']) * da.dtype.itemsize / 1024 / 1024,
+        'open (seconds)': open_time,
+        'load': load_time,
+        'total_time': open_time + load_time
+    }
+    s3_request_info = monitor.get_summary_table()
+    return test_info, s3_request_info
+
+def create_tile(test_file: str, s3fsfs: s3fs.S3FileSystem, random_time: str, xyz: tuple(0,0,0)):
+    monitor = S3FSLogMonitor()
+    
+    test_file_name = test_file.split('/')[-1]
+    print(f"starting test for {test_file_name}")
+    open_start_time = datetime.now()
+    ds = xr.open_dataset(
+        s3fsfs.open(f's3://{test_file}', **s3fs_open_kwargs),
+        **backend_open_kwargs
+    )
+    open_end_time = datetime.now()
+    da = ds.Tair.sel(time=random_time)
+    da.tile(*xyz)
+    tile_time = (datetime.now() - open_end_time).total_seconds()
+    open_time = (open_end_time - open_start_time).total_seconds()
+    ds.close()
+    test_info = {
+        'filename': test_file_name,
+        'chunk_shape': da.encoding['preferred_chunks'],
+        'chunk_size (MB)': np.prod(da.encoding['chunksizes']) * da.dtype.itemsize / 1024 / 1024,
+        'open (seconds)': open_time,
+        'tile': tile_time,
+        'total_time': open_time + tile_time
+    }
+    s3_request_info = monitor.get_summary_table()
+    return test_info, s3_request_info
+    
